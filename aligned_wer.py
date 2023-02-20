ae7892e587942c545ffaa89574e2ea079aaa3836
thibault-roux <thibault.roux1@laposte.net>
2023-02-20 11:00:11 +0100

    ignore useless infos

--- aligned_wer.py
+++ aligned_wer.py
@@ -181,8 +181,8 @@ def wer(r, h):
     list = getStepList(r, h, d)
 
     # print the result in aligned way
-    result = float(d[len(r)][len(h)]) / len(r) * 100
-    result = str("%.2f" % result) + "%"
-    alignedPrint(list, r, h, result)
+    #result = float(d[len(r)][len(h)]) / len(r) * 100
+    #result = str("%.2f" % result) + "%"
+    #alignedPrint(list, r, h, result)
 
     return list, d[len(r)][len(h)]
