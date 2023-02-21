52486f0fd46a07bbb6dbd8cc25daecfc9a378553
thibault-roux <thibault.roux1@laposte.net>
2023-02-21 18:49:38 +0100

    move file

--- visualize.py
+++ visualize.py
@@ -12,4 +12,6 @@ if __name__ == '__main__':
     ref = input("Reference: ")
     hyp = input("Hypothesis: ")
 
-    
+    # find word with error. Insert a token for deletion?
+
+    errors, distance = awer.wer(ref.split(" "), hyp.split(" "))
