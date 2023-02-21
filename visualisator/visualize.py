91bf52c44023fd3400156f72e00f87087a61d9f4
thibault-roux <thibault.roux1@laposte.net>
2023-02-21 18:31:16 +0100

    Create tool for error severity

---
+++ visualisator/visualize.py
@@ -0,0 +1,15 @@
+
+
+# We want to visualize word gravity
+
+# how ?
+# we know that some corrections implies better gains
+
+
+
+
+if __name__ == '__main__':
+    ref = input("Reference: ")
+    hyp = input("Hypothesis: ")
+
+    
